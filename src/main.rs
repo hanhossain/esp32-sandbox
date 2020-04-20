805a54cfbf724d29dae343e5932b5326804bf082
Han Hossain <ahanuf12@gmail.com>
2020-04-19 21:32:55 -0700

    add logging

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,44 @@
 
 extern crate panic_halt;
 
-use xtensa_lx6_rt::delay;
-use esp32_hal::prelude::*;
+use core::fmt::Write;
 use esp32::Peripherals;
+use esp32_hal::clock_control::sleep;
+use esp32_hal::prelude::*;
 
+mod serial;
 mod watchdog;
 
-/// The default clock source is the onboard crystal
-/// In most cases 40mhz (but can be as low as 2mhz depending on the board)
-const CORE_HZ: u32 = 40_000_000;
+const BLINK_HZ: Hertz = Hertz(2);
 
 #[no_mangle]
 fn main() -> ! {
     let dp = unsafe { Peripherals::steal() };
 
-    let mut rtccntl = dp.RTCCNTL;
     let mut timg0 = dp.TIMG0;
     let mut timg1 = dp.TIMG1;
 
-    watchdog::disable_all(&mut rtccntl, &mut timg0, &mut timg1);
+    watchdog::disable_main_system(&mut timg0, &mut timg1);
 
     let pins = dp.GPIO.split();
     let mut led = pins.gpio2.into_open_drain_output();
 
+    let (mut tx, _) = serial::get(
+        dp.UART0,
+        dp.RTCCNTL,
+        dp.APB_CTRL,
+        dp.DPORT
+    );
+
+    writeln!(tx, "ESP32 Started\r").unwrap();
+
     loop {
+        write!(tx, "line1\r\nline2\r\n").unwrap();
+
         led.set_high().unwrap();
-        delay(CORE_HZ);
+        sleep((Hertz(1_000_000) / BLINK_HZ).us());
 
         led.set_low().unwrap();
-        delay(CORE_HZ);
+        sleep((Hertz(1_000_000) / BLINK_HZ).us());
     }
 }
