94c37277b2e33304bd46f600ed6e9476adfe1452
Han Hossain <ahanuf12@gmail.com>
2020-04-19 21:32:13 -0700

    formatting

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -1,22 +1,23 @@
 //! A bunch of watchdog helper methods
 
-use esp32::{RTCCNTL, TIMG0, TIMG1, rtccntl, timg};
+use esp32::{rtccntl, timg, RTCCNTL, TIMG0, TIMG1};
 
 const PROTECT_RESET_VALUE: u32 = 0x50D83AA1;
 
+#[allow(dead_code)]
 pub fn disable_all(rtccntl: &mut RTCCNTL, timg0: &mut TIMG0, timg1: &mut TIMG1) {
     disable_rtc(rtccntl);
     disable_main_system(timg0, timg1);
 }
 
+#[allow(dead_code)]
 /// Disables the RTC Watchdog Timer (RWDT)
 pub fn disable_rtc(rtccntl: &mut RTCCNTL) {
     let wp = WriteProtection::RTC(&rtccntl.wdtwprotect);
     wp.disable();
 
     rtccntl.wdtconfig0.modify(|_, w| unsafe {
-        w
-            .wdt_stg0()
+        w.wdt_stg0()
             .bits(0x0)
             .wdt_stg1()
             .bits(0x0)
@@ -43,12 +44,8 @@ pub fn disable_main_system(timg0: &mut TIMG0, timg1: &mut TIMG1) {
     wp1.disable();
 
     // disable watchdogs
-    timg0
-        .wdtconfig0
-        .write(|w| unsafe { w.bits(0x0) });
-    timg1
-        .wdtconfig0
-        .write(|w| unsafe { w.bits(0x0) });
+    timg0.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
+    timg1.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
 
     // enable write protection
     wp0.enable();
@@ -64,7 +61,9 @@ impl<'a> WriteProtection<'a> {
     fn disable(&self) {
         match self {
             WriteProtection::RTC(reg) => reg.write(|w| unsafe { w.bits(PROTECT_RESET_VALUE) }),
-            WriteProtection::MainSystem(reg) => reg.write(|w| unsafe { w.bits(PROTECT_RESET_VALUE) }),
+            WriteProtection::MainSystem(reg) => {
+                reg.write(|w| unsafe { w.bits(PROTECT_RESET_VALUE) })
+            }
         }
     }
 
