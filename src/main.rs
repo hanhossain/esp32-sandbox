725fc00f612f694e924b731223def2396b309b1b
Han Hossain <ahanuf12@gmail.com>
2020-05-05 00:01:57 -0700

    use updated logger

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 #![no_main]
 
 use core::fmt::Write;
-use esp32::Peripherals;
 use core::panic::PanicInfo;
-use esp32_hal::prelude::*;
-use esp32_logger::{log, STORED_TX};
+use esp32::Peripherals;
 use esp32_hal::clock_control::sleep;
+use esp32_hal::prelude::*;
+use esp32_logger::*;
 
 mod watchdog;
 
@@ -19,12 +19,8 @@ fn main() -> ! {
 
     watchdog::disable_main_system(&mut timg0, &mut timg1);
 
-    esp32_logger::setup(dp.UART0, dp.RTCCNTL, dp.APB_CTRL, dp.DPORT);
+    setup_logger!(dp);
     log!("Hello world from rust!");
-
-    // setup led
-    let pins = dp.GPIO.split();
-    let mut led = pins.gpio2.into_open_drain_output();
 
     let mut counter = 0;
 
@@ -32,10 +28,7 @@ fn main() -> ! {
         log!("counter: {}", counter);
         counter += 1;
 
-        led.set_high().unwrap();
-        sleep(800.ms());
-        led.set_low().unwrap();
-        sleep(200.ms());
+        sleep(1.s());
     }
 }
 
