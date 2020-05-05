fe10d31b4e595c0d2dbe09a21274a9a6e5aa1d56
Han Hossain <ahanuf12@gmail.com>
2020-05-04 23:04:38 -0700

    print panics and attempt to blink led

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
-
 use core::fmt::Write;
 use esp32::Peripherals;
+use core::panic::PanicInfo;
 use esp32_hal::prelude::*;
 use esp32_logger::{log, STORED_TX};
+use esp32_hal::clock_control::sleep;
 
 mod watchdog;
 
@@ -22,9 +22,25 @@ fn main() -> ! {
     esp32_logger::setup(dp.UART0, dp.RTCCNTL, dp.APB_CTRL, dp.DPORT);
     log!("Hello world from rust!");
 
+    // setup led
+    let pins = dp.GPIO.split();
+    let mut led = pins.gpio2.into_open_drain_output();
+
     let mut counter = 0;
+
     loop {
         log!("counter: {}", counter);
         counter += 1;
+
+        led.set_high().unwrap();
+        sleep(800.ms());
+        led.set_low().unwrap();
+        sleep(200.ms());
     }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    log!("{:?}", info);
+    loop {}
 }
