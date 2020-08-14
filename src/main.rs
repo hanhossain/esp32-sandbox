ad0c3357b1295ded2ac57cd0aea1494a6e1c557d
Han Hossain <ahanuf12@gmail.com>
2020-08-14 01:48:02 -0700

    blinky with esp32-hal

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,49 @@
 #![no_std]
 #![no_main]
-#![feature(llvm_asm)]
 
-use esp32_hal::{prelude::*, target};
 use core::panic::PanicInfo;
-
-/// The default clock source is the onboard crystal
-/// In most cases 40mhz (but can be as low as 2mhz depending on the board)
-const CORE_HZ: u32 = 40_000_000;
-
-const BLINKY_GPIO: u32 = 2; // the GPIO hooked up to the onboard LED
-
-const WDT_WKEY_VALUE: u32 = 0x50D83AA1;
+use esp32_hal::{
+    clock_control::{sleep, ClockControl, XTAL_FREQUENCY_AUTO},
+    dport::Split,
+    prelude::*,
+    target,
+    timer::Timer,
+};
 
 #[entry]
 fn main() -> ! {
-    let dp = unsafe { target::Peripherals::steal() };
+    let dp = target::Peripherals::take().expect("failed to acquire peripherals");
+    let (_dport, dport_clock_control) = dp.DPORT.split();
 
-    let mut gpio = dp.GPIO;
-    let mut rtccntl = dp.RTCCNTL;
-    let mut timg0 = dp.TIMG0;
-    let mut timg1 = dp.TIMG1;
+    let clock_control = ClockControl::new(
+        dp.RTCCNTL,
+        dp.APB_CTRL,
+        dport_clock_control,
+        XTAL_FREQUENCY_AUTO,
+    )
+    .unwrap();
 
-    // (https://github.com/espressif/openocd-esp32/blob/97ba3a6bb9eaa898d91df923bbedddfeaaaf28c9/src/target/esp32.c#L431)
-    // openocd disables the wdt's on halt
-    // we will do it manually on startup
-    disable_timg_wdts(&mut timg0, &mut timg1);
-    disable_rtc_wdt(&mut rtccntl);
+    // disable RTC watchdog
+    let (clock_control_config, mut watchdog) = clock_control.freeze().unwrap();
+    watchdog.disable();
 
-    configure_pin_as_output(&mut gpio, BLINKY_GPIO);
+    let (.., mut watchdog0) = Timer::new(dp.TIMG0, clock_control_config);
+    let (.., mut watchdog1) = Timer::new(dp.TIMG1, clock_control_config);
+    watchdog0.disable();
+    watchdog1.disable();
+
+    let gpio = dp.GPIO.split();
+    let mut led = gpio.gpio2.into_push_pull_output();
+
     loop {
-        set_led(&mut gpio, BLINKY_GPIO, true);
-        delay(CORE_HZ);
-        set_led(&mut gpio, BLINKY_GPIO, false);
-        delay(CORE_HZ);
+        led.set_high().unwrap();
+        sleep(500_000.us());
+
+        led.set_low().unwrap();
+        sleep(500_000.us());
     }
 }
 
-fn disable_rtc_wdt(rtccntl: &mut target::RTCCNTL) {
-    /* Disables the RTCWDT */
-    rtccntl
-        .wdtwprotect
-        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
-    rtccntl.wdtconfig0.modify(|_, w| unsafe {
-        w.wdt_stg0()
-            .bits(0x0)
-            .wdt_stg1()
-            .bits(0x0)
-            .wdt_stg2()
-            .bits(0x0)
-            .wdt_stg3()
-            .bits(0x0)
-            .wdt_flashboot_mod_en()
-            .clear_bit()
-            .wdt_en()
-            .clear_bit()
-    });
-    rtccntl.wdtwprotect.write(|w| unsafe { w.bits(0x0) });
-}
-
-fn disable_timg_wdts(timg0: &mut target::TIMG0, timg1: &mut target::TIMG1) {
-    timg0
-        .wdtwprotect
-        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
-    timg1
-        .wdtwprotect
-        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
-
-    timg0.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
-    timg1.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
-}
-
-pub fn set_led(reg: &mut target::GPIO, idx: u32, val: bool) {
-    if val {
-        reg.out_w1ts.modify(|_, w| unsafe { w.bits(0x1 << idx) });
-    } else {
-        reg.out_w1tc.modify(|_, w| unsafe { w.bits(0x1 << idx) });
-    }
-}
-
-/// Configure the pin as an output
-pub fn configure_pin_as_output(reg: &mut target::GPIO, gpio: u32) {
-    reg.enable_w1ts
-        .modify(|_, w| unsafe { w.bits(0x1 << gpio) });
-    reg.func_out_sel_cfg[2]
-        .modify(|_, w| unsafe { w.bits(0x100) });
-}
-
-/// rough delay - as a guess divide your cycles by 20 (results will differ on opt level)
-pub fn delay2(clocks: u32) {
-    let dummy_var: u32 = 0;
-    for _ in 0..clocks {
-        unsafe { core::ptr::read_volatile(&dummy_var) };
-    }
-}
-
-/// cycle accurate delay using the cycle counter register
-pub fn delay(clocks: u32) {
-    // NOTE: does not account for rollover
-    let target = get_ccount() + clocks;
-    loop {
-        if get_ccount() > target {
-            break;
-        }
-    }
-}
-
-/// Performs a special register read to read the current cycle count.
-/// In the future, this can be precompiled to a archive (.a) and linked to so we don't
-/// have to require the asm nightly feature - see cortex-m-rt for more details
-pub fn get_ccount() -> u32 {
-    let x: u32;
-    unsafe { llvm_asm!("rsr.ccount a2" : "={a2}"(x) ) };
-    x
-}
-
-/// Basic panic handler - just loops
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
