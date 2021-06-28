da8c79b83e4e38856760abbc4eb2e556127af18f
Han Hossain <ahanuf12@gmail.com>
2021-06-28 00:18:38 -0700

    get temperature reading from heltec lora board with lm35

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ use core::panic::PanicInfo;
 use esp32_hal::{
     clock_control::{sleep, ClockControl, XTAL_FREQUENCY_AUTO},
     dport::Split,
+    dprintln,
     prelude::*,
     target,
     timer::Timer,
 };
-use log::{error, info, LevelFilter};
+use esp32_hal::analog::config::{Adc1Config, Attenuation};
+use esp32_hal::analog::adc::ADC;
+use esp32_hal::target::EFUSE;
+use esp32_hal::efuse::Efuse;
 
 #[entry]
 fn main() -> ! {
@@ -34,20 +38,36 @@ fn main() -> ! {
     watchdog0.disable();
     watchdog1.disable();
 
-    esp32_logger::init_with_level(LevelFilter::Trace);
+    let gpios = dp.GPIO.split();
+    let mut pin = gpios.gpio36.into_analog();
+    let mut adc_config = Adc1Config::new();
+    adc_config.enable_pin(&pin, Attenuation::Attenuation11dB);
 
-    let mut counter = 0;
+    let analog = dp.SENS.split();
+    let mut adc = ADC::adc1(analog.adc1, adc_config).unwrap();
 
     loop {
-        counter += 1;
-
-        info!("Counter: {}", counter);
+        let raw: u16 = nb::block!(adc.read(&mut pin)).unwrap();
+        let reading = convert_to_volts(raw);
+        convert_to_fahrenheit(reading);
         sleep(1.s());
     }
 }
 
+fn convert_to_fahrenheit(reading: f32) {
+    let celsius = reading / 0.01;
+    let fahrenheit = celsius * 1.8 + 32.0;
+    dprintln!("fahrenheit: {}, celsius: {}", fahrenheit, celsius);
+}
+
+fn convert_to_volts(value: u16) -> f32 {
+    // for some reason, the adc reading in the heltec = 4095 - value.
+    let reading = 4095 - value;
+    4.96 * reading as f32 / 4095.0
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    error!("{:?}", info);
+    dprintln!("PANIC: {:?}", info);
     loop {}
 }
